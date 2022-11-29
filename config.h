7f0a2ec4a01c9a31e793e29f417c84c69166aec7
Prajith PA <codejs100@gmail.com>
2022-11-29 06:20:48 -0800

    adding changes

--- config.h
+++ config.h
@@ -99,29 +99,29 @@ float alpha = 0.8;
 const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#1e2127", /* black   */
-  [1] = "#e06c75", /* red     */
-  [2] = "#98c379", /* green   */
-  [3] = "#d19a66", /* yellow  */
-  [4] = "#61afef", /* blue    */
-  [5] = "#c678dd", /* magenta */
-  [6] = "#56b6c2", /* cyan    */
-  [7] = "#828791", /* white   */
+  [0] = "#32344a", /* black   */
+  [1] = "#f7768e", /* red     */
+  [2] = "#9ece6a", /* green   */
+  [3] = "#e0af68", /* yellow  */
+  [4] = "#7aa2f7", /* blue    */
+  [5] = "#ad8ee6", /* magenta */
+  [6] = "#449dab", /* cyan    */
+  [7] = "#787c99", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#5c6370",  /* black   */
-  [9]  = "#e06c75",  /* red     */
-  [10] = "#98c379", /* green   */
-  [11] = "#d19a66", /* yellow  */
-  [12] = "#61afef", /* blue    */
-  [13] = "#c678dd", /* magenta */
-  [14] = "#56b6c2", /* cyan    */
-  [15] = "#e6efff", /* white   */
+  [8]  = "#444b6a",  /* black   */
+  [9]  = "#ff7a93",  /* red     */
+  [10] = "#b9f27c", /* green   */
+  [11] = "#ff9e64", /* yellow  */
+  [12] = "#7da6ff", /* blue    */
+  [13] = "#bb9af7", /* magenta */
+  [14] = "#0db9d7", /* cyan    */
+  [15] = "#acb0d0", /* white   */
 
   /* special colors */
-  [256] = "#282c34", /* background */
-  [257] = "#9dd9d2", /* foreground */
-  [258] = "#9dd9d2",     /* cursor */
+  [256] = "#000000", /* background */
+  [257] = "#7aa2f7", /* foreground */
+  [258] = "#bb9af7",     /* cursor */
 };
 
 /* Default colors (colorname index)
